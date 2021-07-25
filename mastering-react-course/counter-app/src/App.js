90e1c425747dfcb94174b0fa03e11129cb1cdb95
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-07-25 00:00:14 -0700

    create counter app

---
+++ mastering-react-course/counter-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Derikito Takito
+        </p>
+        <a
+          className="Click Here"
+          href="https://youtu.be/dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
