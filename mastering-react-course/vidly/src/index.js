635f5bf70cf491d2c23cd81162550c7d24f82e7c
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-07-25 21:12:45 -0700

    counter

--- mastering-react-course/vidly/src/index.js
+++ mastering-react-course/vidly/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-
-import Counter from './components/counter'
 
 ReactDOM.render(
-  <Counter />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
